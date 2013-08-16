f0666a6a30fc38c985df60cdd8b335ecc5632b2c
Omid Gharib <omidgharib@yahoo.com>
2013-08-16 17:24:08 +0430

    clear all

--- js/com/sakurapixel/events/genericevents.js
+++
@@ -1,8 +0,0 @@
-
-function Event(evType, data){
-	this.eventType = evType;
-	this.data = data;
-}
-Event.COMPLETE = "COMPLETE";
-Event.WINDOW_RESIZE = "WINDOW_RESIZE";
-Event.WINDOW_LOCATION_CHANGE = "WINDOW_LOCATION_CHANGE";
