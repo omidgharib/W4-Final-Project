70b9f6c751cb47e4ccfc05b639ba062d3e52955c
Omid Gharib <omidgharib@yahoo.com>
2013-08-22 07:59:28 +0430

    Add Member post type

--- WP/functions.php
+++ WP/functions.php
@@ -5,6 +5,39 @@ function custome_theme_init (){
 	add_theme_support('menus');
 	register_nav_menu('mainmenu', "Main Manu");
 	add_theme_support('post-thumbnails');
+	
+	$labels = array(
+	    'name' => _x('Member', 'post type general name'),
+	    'singular_name' => _x('Member', 'post type singular name'),
+	    'add_new' => _x('Add New', 'Member'),
+	    'add_new_item' => __('Add New Member'),
+	    'edit_item' => __('Edit Member'),
+	    'new_item' => __('New Member'),
+	    'view_item' => __('View Member'),
+	    'search_items' => __('Search Member'),
+	    'not_found' =>  __('No Member found'),
+	    'not_found_in_trash' => __('No Member found in Trash'),
+	    'parent_item_colon' => '',
+	    'menu_name' => _x('Member', 'post type general name')
+	);
+
+	$args = array(
+	    'labels' => $labels,
+	    'public' => true,
+	    'publicly_queryable' => true,
+	    'show_ui' => true,
+	    'show_in_menu' => true,
+	    'query_var' => true,
+	    'rewrite' => true,
+	    'capability_type' => 'post',
+	    'has_archive' => true,
+	    'hierarchical' => false,
+	    'menu_position' => 20,
+	    //'menu_icon' => get_bloginfo('template_url') . '/images/menu.png', // 16x16
+	    'supports' => array('title','editor','thumbnail','excerpt','custom-fields')
+	);
+
+	register_post_type ('Member',$args);
 }
 
 add_action('init', 'custome_theme_init');
