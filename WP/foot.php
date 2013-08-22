b85e9ba464edc7d5ec99d5e5415bc18afa09d1d2
Omid Gharib <omidgharib@yahoo.com>
2013-08-22 07:07:08 +0430

    fixed a bug :D

--- WP/foot.php
+++ WP/foot.php
@@ -1,23 +1,5 @@
-<div class="containers cover bgred">
-	<footer>
-		<div class="info">
-			<div class="social-links cover">
-				<a class="github" href="https://github.com/omidgharib"></a>
-				<a class="facebook" href="http://www.facebook.com/people/@/omidgharib"></a>
-				<a class="linkedin" href="http://www.linkedin.com/pub/omid-gharib/37/119/a41"></a>
-			</div>
-			<address class='cover'>
-				<span>با من در ارتباط باشید </span><br />
-				<span>ایمیل: <a href="mailto:omidgharib@yahoo.com">omidgharib@yahoo.com</a></span><br />
-				<span>شماره تلفن: 989360775700+</span>
-			</address>
-			<div class="clear"></div>
-		</div>
-		<a class='logo' href="#">
-			<h1>
-				<div class='logoimg'></div>
-				<span>کلاس جهانی طراحی وب</span>
-			</h1>
-		</a>
-	</footer>
-</div>
+<!-- <script src="//ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js"></script> -->
+<script src="./scripts/jquery.min.js"></script>
+<script src="./scripts/script.js"></script>
+</body>
+</html>
